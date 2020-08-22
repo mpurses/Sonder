ed2aea6506050bcb52d59c38ae19d1c646e3026b
mpurses <mpurses@gmail.com>
2020-08-22 15:17:39 -0500

    2.5.4 changes
    ;- Fix Weather - New way to automatically get your location and lot's of code changes underneath so please report any bugs you encounter. Switch to use Weather.com API (jsmorley's code)
    ;- Visualizer: New Line-based Visualizer
    ;- Calendar: Adjust Month Font sizes to be slightly smaller so you can put the calendar on left edge of screen
    ;- Calendar: Vertical "Y" adjustment option (For the smaller fonts)
    ;- Date: Fix Date_Handwriting skin's month font location/size
    ;- More Font options for Time/Date/Calendar:
    ; - Sterilict (Spacey/Futuristic),
    ; - BankGothic Lt BT (Spacey/Futuristic),
    ; - Papyrus (Japanesey/Serenity),
    ; - Voluta Script Pro (Cursive/Firefly)

--- Skins/Sonder/@Resources/Visualizer/MeasureGenerator.lua
+++ Skins/Sonder/@Resources/Visualizer/MeasureGenerator.lua
@@ -101,3 +101,47 @@ function MeasureGeneratorBar(band,preview)
 	end
 	file:close()
 end
+
+function MeasureGeneratorLine(band,preview)
+	SKIN:Bang('!WriteKeyValue Audio Bands '..band+1)
+	local file = io.open(SKIN:GetVariable('@')..'Visualizer\\MeasureAudio.inc','w')
+	math.randomseed(1234)
+	
+	for i = 0,band do
+		if preview == 0 then
+			file:write('[MeasureAudio'..i..']\n',
+						'Measure=Plugin\n',
+						'Group=Audio\n',
+						'Plugin=AudioLevel\n',
+						'Parent=Audio\n',
+						'Type=Band\n',
+						'Channel=Sum\n',
+						'BandIdx='..i..'\n',
+						'AverageSize=#LineReactionAverage#\n',
+						'UpdateDivider=#UpdateSpeed#\n',
+						'DynamicVariables=1\n')
+		else
+			file:write('[MeasureAudio'..i..']\n',
+						'Measure=Calc\n',
+						'Formula='..math.random()..'\n')
+		end
+	end
+
+	file:write('[MeterGradientBar]\n',
+				'Meter=Shape\n',
+				'X=5\n',
+				'Y=(#LineHeight#/2)\n',
+				'Shape=Path MyPath | StrokeWidth #LineThickness# | Stroke LinearGradient StrokeGradient\n',
+				'Shape2=Path MyPath | StrokeWidth #LineThickness# | Stroke LinearGradient StrokeGradient\n',
+				'StrokeGradient=#GradientOrientation# | #VisualizerColor1# ; 0.25 | #VisualizerColor2# ; 0.85\n',
+				'DynamicVariables=1\n',
+				'AntiAlias=1\n',
+				'UpdateDivider=#UpdateSpeed#\n',
+				'MyPath=0,0 | CurveTo #LineWidth#,0,(#LineWidth#/2),(#LineHeight#*[MeasureAudio1]),0')
+	dir = -1
+	for i = 2,band do
+		file:write(' | CurveTo (#LineWidth#*'..i..'),0,((#LineWidth#*'..i..')-(#LineWidth#/2)),('..dir..'*#LineHeight#*[MeasureAudio'..i..']),0')
+		dir=(dir*-1)
+	end
+	file:close()
+end
