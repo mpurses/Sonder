d094c5334d68ce7ffe84b457916bbb639fed83e9
mpurses <mpurses@gmail.com>
2021-01-03 11:48:28 -0600

    silent update for 21.1

--- Skins/Sonder/@Resources/Scripts/ToDoNotes.lua
+++ Skins/Sonder/@Resources/Scripts/ToDoNotes.lua
@@ -5,12 +5,14 @@ end
 function EditItemA()
 	SKIN:Bang('!SetVariable ItemOrig """'..SKIN:GetVariable('ToDoText'):gsub("\n", "\r\n")..'"""')
 	SKIN:Bang('!CommandMeasure ToDoTextBoxInput "ExecuteBatch 1"')
+	--SKIN:Bang('[!CommandMeasure AddBullet Start]')
 end
 
 function EditItemB()
 	local input = SKIN:GetVariable('Input'):gsub("\r\n", "#*CRLF*#")
 	if input ~= "" then
 		SKIN:Bang('[!WriteKeyValue Variables ToDoText """'..input..'""" "#@#Variables.inc"]')
+		--SKIN:Bang('[!CommandMeasure AddBullet Stop]')
 	else
 		DeleteItem()
 	end
